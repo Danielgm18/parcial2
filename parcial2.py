0ecae1be4e626a30b92830720e5082c5b057b5a7
Danielgm18 <yhgomezm@unal.edu.co>
2025-11-10 12:13:16 -0500

    first commit

---
+++ parcial2.py
@@ -0,0 +1,31 @@
+#1.Desarrollar un programa que determine si en una lista no existen elementos repetidos
+lista=[1, 2, 3, 4, 5, 6, 6]
+def ejercicio1():
+  for i in range(8):
+    if (lista[0:7:1])==(lista[1:7:1]):
+      print("Hay elementos repetidos")
+    else:
+      print("No hay elementos repetidos")
+#2.Desarrollar un programa que determine si en una lista se encuentra una cadena de caracteres con 2 o mas vocales. 
+#Si en la cadena existe debe imprimirla y si no existe imprime no existe
+lista1=["Cali","Uno", "o"]
+def ejercicio2():
+  for i in range(len(lista1)):
+    if lista1.count():
+        break
+print(lista1.values())
+'''
+#3.Desarrollar un programa que dada 2 listas determine que elementos tiene la primera lista que no tenga la segunda lista 
+lista2=[1,2,3,5,7]
+lista3=[2,4,6,7,8]
+
+
+#4.DESAROLLAR UN ALGORITMO QUE CALCULE EL PROMEDIO DE UN ARREGLO DE REALES
+#def main():
+# #ejercicio1())
+#if __name__== __name__:
+  #main()
+
+if 1==1:
+  ejercicio1()
+'''
